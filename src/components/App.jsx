3e8801dee3d75cb5fd67efd8fc20467d076a5ce4
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-05 15:40:51 +0200

    початок

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import user from '../data/user.json';
 import data from '../data/data.json'
 import transactions from '../data/transactions.json';
+// import friends from '../data/friends.json';
 import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
 import { TransactionHistory } from './transactionHistory/TransactionHistory';
-
-console.log("🚀 ~ Profile:", Profile);
-console.log("🚀 ~ user:", user);
+// import { FriendList } from './friendList/FriendList'
 
 export const App = () => {
   return (<>
     <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
     <Statistics title="Upload stats" stats={data} />
     <TransactionHistory items={transactions} />
+    {/* <FriendList friends={friends} /> */}
   </>);
 };
--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,6 +1,6 @@
 
 function FriendListItem({ avatar, name, isOnline, id }) {
-    return <li key={id} className="item">
+    return <li className="item" key={id}>
         <span className="status">{isOnline}</span>
         <img className="avatar" src={avatar} alt="User avatar" width="48" />
         <p className="name">{name}</p>
@@ -9,10 +9,8 @@ function FriendListItem({ avatar, name, isOnline, id }) {
 
 export const FriendList = ({ friends }) => {
     return <ul className="friend-list">
-        {friends.map(({ avatar, name, isOnline, id }) => {
-            return <FriendListItem avatar={avatar} name={name} isOnline={isOnline} id={id} />
+        {friends.map(fre => {
+            return <FriendListItem avatar={fre.avatar} name={fre.name} isOnline={fre.isOnline} id={fre.id} />;
         })}
     </ul >
 }
-
-

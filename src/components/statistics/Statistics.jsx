281e8014cbda34ded6c634a8f2eba5c04eb7d291
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-05 17:42:21 +0200

    початкові стилі статистики

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import statCSS from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
-    return <section className={statCSS.statistics}> <h2>{title}</h2> <ul className="stat-list">
+    return <section className={statCSS.statistics}> <h2>{title}</h2> <ul className={statCSS.statList}>
         {stats.map(({ label, percentage, id }) => {
             return <li className="item" key={id}>
                 <span className="label">{label}</span>

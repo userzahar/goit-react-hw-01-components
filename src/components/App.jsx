e82205f63f52483aa240c70bb2a8c50001ed7baa
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-05 14:40:22 +0200

    ок якщо так?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,6 @@ console.log("🚀 ~ user:", user);
 export const App = () => {
   return (<>
     <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+
   </>);
 };
-
-
-{/* <div
-  style={{
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: 40,
-    color: '#010101'
-  }}
->
-  React homework template
-</div> */}

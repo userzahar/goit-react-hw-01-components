619a3ff02604e76a188f127322e5e0710651f2f0
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-05 18:26:41 +0200

    Прекрасно зробив статистику

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -3,15 +3,15 @@ export const TransactionHistory = ({ items }) => {
     return <> <table className={transCss.transactionHistory}>
         <thead>
             <tr>
-                <th>Type</th>
-                <th>Amount</th>
-                <th>Currency</th>
+                <th className={transCss.head}>Type</th>
+                <th className={transCss.head}>Amount</th>
+                <th className={transCss.head}>Currency</th>
             </tr>
         </thead>
 
         <tbody>
             {items.map((item) => {
-                return <tr key={item.id}>
+                return <tr className={transCss.body} key={item.id}>
                     <td>{item.type}</td>
                     <td>{item.amount}</td>
                     <td>{item.currency}</td>

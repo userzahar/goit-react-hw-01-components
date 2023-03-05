0c3b935891f8d0b523882d124880ba8027c376aa
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-05 18:50:36 +0200

    рандомні кольори

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,11 +1,12 @@
 import statCSS from './Statistics.module.css';
+import { getRandomColor } from '../functions/randomColor';
 export const Statistics = ({ title, stats }) => {
     return <section className={statCSS.statistics}> <h2>{title}</h2> <ul className={statCSS.statList}>
         {stats.map(({ label, percentage, id }) => {
-            return <li className="item" key={id}>
+            return <li className="item" style={{ backgroundColor: getRandomColor() }} key={id}>
                 <span className="label">{label}</span>
                 <span className="percentage">{percentage}%</span>
             </li>
         })}
-    </ul></section>
+    </ul></section >
 }

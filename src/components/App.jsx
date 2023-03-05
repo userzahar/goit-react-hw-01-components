3fc28e16ebd00b21cab844fee1b3811925966d10
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-05 14:42:02 +0200

    а так?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,3 +10,17 @@ export const App = () => {
 
   </>);
 };
+
+// лщлщлщл
+/* <div
+  style={{
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 40,
+    color: '#010101'
+  }}
+>
+  React homework template
+</div> */

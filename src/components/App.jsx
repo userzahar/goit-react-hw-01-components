93ce43b32534b306682b2fd9dc337c89e93ecb4a
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-05 14:36:45 +0200

    забрати лишнє?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,19 @@ console.log("🚀 ~ user:", user);
 export const App = () => {
   return (<>
     <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-
   </>);
 };
+
+
+{/* <div
+  style={{
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 40,
+    color: '#010101'
+  }}
+>
+  React homework template
+</div> */}

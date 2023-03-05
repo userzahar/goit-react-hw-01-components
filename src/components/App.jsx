5c8b3e1a3a06bdbe38173a152ac9b1c79832c79f
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-05 14:51:47 +0200

    items transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import user from '../data/user.json';
 import data from '../data/data.json'
+import transactions from '../data/transactions.json';
 import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
 
 console.log("🚀 ~ Profile:", Profile);
 console.log("🚀 ~ user:", user);
@@ -10,5 +12,6 @@ export const App = () => {
   return (<>
     <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
     <Statistics title="Upload stats" stats={data} />
+    <TransactionHistory items={transactions} />
   </>);
 };
